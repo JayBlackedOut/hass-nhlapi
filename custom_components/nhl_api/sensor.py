6db65563275800d559b70d7545dd17b561a93171
JayBlackedOut <jasonlowenthal@live.com>
2019-10-05 19:35:08 -0400

    Correct state setting

--- custom_components/nhl_api/sensor.py
+++ custom_components/nhl_api/sensor.py
@@ -106,7 +106,7 @@ class NHLSensor(Entity):
         if plays.get('game_state') == "Scheduled":
             self._state = next_date_time
         else:
-            self.state = plays.get('game_state', next_date_time)
+            self._state = plays.get('game_state', next_date_time)
         # Set sensor state attributes.
         self._state_attributes = all_attr
         # Set away team logo url as attribute 'away_logo'.
