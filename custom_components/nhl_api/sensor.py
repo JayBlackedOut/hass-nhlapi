32657ec39507e291734b9a1c528a8b1cc9f84685
JayBlackedOut <jasonlowenthal@live.com>
2021-03-05 12:48:28 -0500

    Bump version to 0.8.1

--- custom_components/nhl_api/sensor.py
+++ custom_components/nhl_api/sensor.py
@@ -18,7 +18,7 @@ from homeassistant.helpers.event import track_point_in_time
 
 _LOGGER = logging.getLogger(__name__)
 
-__version__ = '0.8.0'
+__version__ = '0.8.1'
 
 CONF_ID = 'team_id'
 CONF_NAME = 'name'
