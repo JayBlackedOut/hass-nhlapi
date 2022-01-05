224d73e981313ec288ef94148d671a29a3eb30a4
Mikal Houston <mikalhouston@gmail.com>
2022-01-04 21:42:39 -0500

    Remove deprecated device_state_attributes
    Due to a change in HA 2021.12 :
    home-assistant/core#47304
    remove deprecated device_state_attributes in favor of extra_state_attributes.
    Fixes JayBlackedOut/hass-nhlapi#45

--- custom_components/nhl_api/sensor.py
+++ custom_components/nhl_api/sensor.py
@@ -88,7 +88,7 @@ class NHLSensor(Entity):
         return self._state
 
     @property
-    def device_state_attributes(self):
+    def extra_state_attributes(self):
         """Return the state attributes of the sensor."""
         return self._state_attributes
 
