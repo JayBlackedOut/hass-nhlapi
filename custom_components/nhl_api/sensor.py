c2882544ff97d2b88583f43103c0590652ef8d49
Jay <jayblackedout@gmail.com>
2025-10-01 23:54:30 -0400

    bump to 0.13.3

--- custom_components/nhl_api/sensor.py
+++ custom_components/nhl_api/sensor.py
@@ -18,7 +18,7 @@ from homeassistant.helpers.event import track_point_in_time
 
 _LOGGER = logging.getLogger(__name__)
 
-__version__ = '0.13.2'
+__version__ = '0.13.3'
 
 CONF_ABBREV = 'team_abbrev'
 CONF_NAME = 'name'
